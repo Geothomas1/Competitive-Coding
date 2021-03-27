297c5953868414c87fb2abc52f92a0225e0f5778
Geo Thomas <geothomas@cet.ac.in>
2021-03-27 19:55:18 +0530

    change

--- player.py
+++ player.py
@@ -9,13 +9,20 @@ class Player:
 class Team:
     def __init__(self,player):
         self.player=player
-    def getminruns(player):
-        #complete code 
-
-    def getmaxwicket(player):
-    #     complete code 
-    
-
+    def getminruns(self,player):
+         minrun=0
+         for i in player:
+             if i.runsc<minrun:
+                 minrun=i.minrun
+                 k=i
+                 return k
+    def getmaxwicket(self,player): 
+        maxwicket=0
+        for i in player:
+            if i.wicketc>maxwicket:
+                maxwicket=i.wicketc
+                k=i
+                return k    
 n=int(input())
 player=[]
 for i in range(n):
@@ -29,6 +36,8 @@ for i in range(n):
 
 t=Team(player)
 f1=t.getminruns(player)
+print(f1.name)
 #print player data
 f2=t.getmaxwicket(player) 
+print(f2.name)
 #print player data
