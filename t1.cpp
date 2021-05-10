db6d9477ab5a1096bcad13f437f32be745c4749a
Geo Thomas <geothomas@cet.ac.in>
2021-05-10 07:55:17 +0530

    ignore class

---
+++ t1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sumto(int a,int b)
+{
+    return a+b;
+}
+int main()
+{
+   int a,b;
+   cin>>a;
+   cin>>b;
+   int c =sumto(a,b);
+   cout<<c;
+   return 0;
+}
